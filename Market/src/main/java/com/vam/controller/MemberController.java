bea777d8b70735946937173b0de29b0c95881850
박용호 <qkaxhf8823@naver.com>
2023-11-14 13:03:46 +0900

    메인페이지 완성, 로그인,회원가입 페이지 이동

--- Market/src/main/java/com/vam/controller/MemberController.java
+++ Market/src/main/java/com/vam/controller/MemberController.java
@@ -2,14 +2,33 @@ package com.vam.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.vam.VO.MemberVO;
+import com.vam.service.MemberService;
 
 @Controller
 @RequestMapping(value = "/member")
 public class MemberController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(MemberController.class);
-
 	
+	@Autowired
+	MemberService memberservice;
+
+    @Autowired // 암호화 의존성주입
+    private BCryptPasswordEncoder pwEncoder;
+	@RequestMapping(value="/join" , method = RequestMethod.GET)
+	public void memberJoin() throws Exception{
+		logger.info("memberJoin 진입");
+	}
+	
+	@RequestMapping(value="/login", method=RequestMethod.GET )
+	public void memberLogin() throws Exception{
+		logger.info("memberLogin 진입");
+	}
 }
