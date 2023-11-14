1b7f441c595deac8f827cc2a492107a7bf6aca33
박용호 <qkaxhf8823@naver.com>
2023-11-14 17:00:43 +0900

    no message

--- Market/src/main/java/com/vam/controller/MemberController.java
+++ Market/src/main/java/com/vam/controller/MemberController.java
@@ -22,11 +22,19 @@ public class MemberController {
 
     @Autowired // 암호화 의존성주입
     private BCryptPasswordEncoder pwEncoder;
+    // 회원가입 버튼 클릭 시
 	@RequestMapping(value="/join" , method = RequestMethod.GET)
 	public void memberJoin() throws Exception{
 		logger.info("memberJoin 진입");
 	}
+	// 회원가입
+	@RequestMapping(value="/join" ,method= RequestMethod.POST)
+	public String joinPost(MemberVO mvo) throws Exception{
+		String rawPw =""; // 인코딩 전
+		return "redirect:/login";
+	}
 	
+	// 로그인 버튼 클릭 시
 	@RequestMapping(value="/login", method=RequestMethod.GET )
 	public void memberLogin() throws Exception{
 		logger.info("memberLogin 진입");
